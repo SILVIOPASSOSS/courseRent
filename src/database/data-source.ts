4de36ab0e70a0b7f067f53b896e5ef05a706835b
SILVIOPASSOSS <silvyopassosdossantos@hotmail.com>
2024-01-18 19:26:08 -0300

    feat: connection with the bank

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,7 +1,15 @@
-import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-const connectionSource = new DataSource({
+import { User } from "../modules/accounts/entities/User";
+import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
+import { CreateCategories1705235130781 } from "./migrations/1705235130781-CreateCategories";
+import { CreateSpecifications1705264658151 } from "./migrations/1705264658151-CreateSpecifications";
+import { CreateUsers1705309978493 } from "./migrations/1705309978493-CreateUsers";
+import { AlterUserDeleteUsername1705397228434 } from "./migrations/1705397228434-AlterUserDeleteUsername";
+import { AlterUserAddAvatar1705481662082 } from "./migrations/1705481662082-AlterUserAddAvatar";
+
+const dataSource = new DataSource({
     type: "postgres",
     port: 5432,
     username: "docker",
@@ -9,15 +17,17 @@ const connectionSource = new DataSource({
     database: "rentx",
     synchronize: false,
     logging: false,
-    entities: ["src/database/entities/**/*.entity{.ts,.js}"],
-    migrations: ["src/database/migrations/**/*{.tsm,.js}"],
-    subscribers: ["src/database/subscribers/**/*{.ts,.js}"],
-    cli: {
-        migrationsDir: "./src/database/migrations",
-    },
+    entities: [User, Category, Specification],
+    migrations: [
+        CreateCategories1705235130781,
+        CreateSpecifications1705264658151,
+        CreateUsers1705309978493,
+        AlterUserDeleteUsername1705397228434,
+        AlterUserAddAvatar1705481662082,
+    ],
 });
 
 export function createConnection(host = "database"): Promise<DataSource> {
-    return connectionSource.setOptions({ host }).initialize();
+    return dataSource.setOptions({ host }).initialize();
 }
-export default connectionSource;
+export default dataSource;
